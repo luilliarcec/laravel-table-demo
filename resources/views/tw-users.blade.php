5a767bfef6602cbc73d9ba391dcb9fe70b562c1c
Luis Arce <luilliarcec@gmail.com>
2021-06-30 06:54:21 -0500

    Tailwind

--- resources/views/tw-users.blade.php
+++ resources/views/tw-users.blade.php
@@ -8,8 +8,7 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     <style>
         body {
