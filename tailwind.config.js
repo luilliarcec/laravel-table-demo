fe651332227901ba4ea7b83e1282f22da29856f1
Luis Arce <luilliarcec@gmail.com>
2022-04-06 07:04:02 -0500

    use tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,5 +32,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [require('@tailwindcss/forms')],
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.postCss('resources/css/app.css', 'public/css', [
-        require('tailwindcss'),
-    ])
-
-if (mix.inProduction()) {
-    mix.version();
-}
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);
