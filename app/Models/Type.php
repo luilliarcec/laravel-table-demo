18eb7116c5abe7c91ab840890f3c1bab1ddb0bc8
Ubaldo <ubaldocoronel@gmail.com>
2022-04-07 16:54:31 -0500

    Create model, factory and seeder from types

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class type extends Model
+class Type extends Model
 {
     use HasFactory;
 
@@ -16,6 +16,6 @@ class type extends Model
 
     public function users()
     {
-        return $this->belongsTo(User::class);
+        return $this->hasMany(User::class);
     }
 }
