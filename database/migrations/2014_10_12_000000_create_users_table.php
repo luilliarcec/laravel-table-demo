583dfb03fefe8631765f8749a2ed13c5a0c76cdf
Luis Arce <luilliarcec@gmail.com>
2022-06-24 02:54:12 -0500

    WIP

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('language_developer')->nullable();
+            $table->text('language_developer')->nullable();
             $table->float('salary')->nullable();
             $table->boolean('home_office')->default(false);
             $table->rememberToken();
