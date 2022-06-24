cb556b1e8b8322569562cb40d18fb1f8315baf79
Luis Arce <luilliarcec@gmail.com>
2022-06-24 03:09:39 -0500

    WIP

--- config/table.php
+++ config/table.php
@@ -1,33 +1,16 @@
 <?php
 
-use Luilliarcec\LaravelTable\View\Components;
-
 return [
-    /** tailwind  / bootstrap4 / bootstrap5 */
-    'theme' => 'bootstrap5',
+    'dark_mode' => true,
 
-    'components' => [
-        // Filters
-        Components\Filters\Text::class => 'filters.text',
-        Components\Filters\Date::class => 'filters.date',
-        Components\Filters\DateRange::class => 'filters.date-range',
-        Components\Filters\Checkbox::class => 'filters.checkbox',
-        Components\Filters\Select::class => 'filters.select',
-        Components\Filters\SelectMultiple::class => 'filters.select-multiple',
+    'layout' => [
+        'action_alignment' => 'right',
+        'default_pagination_view' => 'tables::components.pagination.index'
+    ],
 
-        // Tables
-        Components\Table::class => 'table',
-        Components\EmptyTable::class => 'empty-table',
-        Components\TableWrapper::class => 'table-wrapper',
-        Components\Td::class => 'td',
-        Components\Th::class => 'th',
-        Components\GlobalSearch::class => 'global-search',
-        Components\Filters::class => 'filters',
-        Components\Columns::class => 'columns',
-        Components\ActionButton::class => 'action-button',
-
-        // Assets
-        Components\Scripts::class => 'scripts',
-        Components\Styles::class => 'styles',
-    ]
+    'formats' => [
+        'date' => 'M j, Y',
+        'datetime' => 'M j, Y H:i:s',
+        'time' => 'H:i:s',
+    ],
 ];
