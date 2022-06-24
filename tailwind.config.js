bff5b5d380f9eb0bf80a148c568da81bd7d5f137
Luis Arce <luilliarcec@gmail.com>
2022-06-24 03:16:02 -0500

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     darkMode: 'class',
 
     purge: [
-        './node_modules/@protonemedia/inertiajs-tables-laravel-query-builder/**/*.{js,vue}',
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
