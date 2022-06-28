266fd69b5c8b0031b1cb5e48f503f78f57ebdada
Luis Arce <luilliarcec@gmail.com>
2022-06-28 08:04:25 -0500

    Factories

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JobFactory extends Factory
+{
+    protected $model = Job::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+        ];
+    }
+}
